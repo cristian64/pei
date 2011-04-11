d97483a5a743e64f0649708af7dbe1bf00a7cb0b
cristian64@gmail.com <cristian64@gmail.com@928e6174-d238-cfd1-98c4-7db66ba74160>
2011-04-11 18:19:58 +0000

    CMakeLists actualizado para Gtkmm y Glademm (con ventana de ejemplo incluida).

--- Practica-pei/Codigo/Vista-gtkmm/main.cpp
+++ Practica-pei/Codigo/Vista-gtkmm/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 using namespace std;
 
-#include "Profesor.h"
+#include <libglademm/xml.h>
+#include <gtkmm.h>
 
-int main()
+int main (int argc, char **argv)
 {
-	Profesor profesor;
+    Gtk::Main app (argc, argv);
+    Glib::RefPtr<Gnome::Glade::Xml> refXml;
+    Gtk::Window* ventana = NULL;
+
+    try
+    {
+        refXml = Gnome::Glade::Xml::create("interfaces.glade");
+
+        refXml->get_widget("MainWindow", ventana);
+        if (ventana)
+        {
+            app.run(*ventana);
+            delete ventana;
+        }
+    }
+    catch (const Gnome::Glade::XmlError& ex)
+    {
+        std::cerr << ex.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
+
