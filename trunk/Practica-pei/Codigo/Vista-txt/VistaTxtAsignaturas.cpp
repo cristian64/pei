76cdd773f7cf032f22f76b64b86b59b6d3f53e11
cristian64@gmail.com <cristian64@gmail.com@928e6174-d238-cfd1-98c4-7db66ba74160>
2011-04-13 17:22:15 +0000

    * Añadido un fichero de ejemplo con asignaturas y todo eso. * Mejorada la vista de texto. La vista de asignaturas asigna internamente una vista para cada asignatura particular. Cuando se refresca una asignatura, ya tiene una vista de texto asignada. Asumiendo que sólo un objeto Asignaturas se creará en cada iteración, cada vez que se refresque, se crea una vista para cada asignatura que contenga. Después, si esas asignaturas se refrescan, se escribirá por pantalla.

--- trunk/Practica-pei/Codigo/Vista-txt/VistaTxtAsignaturas.cpp
+++ trunk/Practica-pei/Codigo/Vista-txt/VistaTxtAsignaturas.cpp
@@ -5,11 +5,31 @@
 
 VistaTxtAsignaturas::VistaTxtAsignaturas(Asignaturas *asignaturas)
 {
-    ponerModelo(asignaturas);
+    if (asignaturas != NULL)
+    {
+        ponerModelo(asignaturas);
+        std::list<Asignatura*>::const_iterator i = asignaturas->obtenerAsignaturas().begin();
+        for (; i != asignaturas->obtenerAsignaturas().end(); i++)
+        {
+            VistaTxtAsignatura *vistaTxtAsignatura = new VistaTxtAsignatura(*i);
+            vistasTxtAsignatura.push_back(vistaTxtAsignatura);
+        }
+    }
+}
+
+VistaTxtAsignaturas::~VistaTxtAsignaturas()
+{
+    for (std::list<VistaTxtAsignatura*>::iterator i = vistasTxtAsignatura.begin(); i != vistasTxtAsignatura.end(); i++)
+        delete *i;
+    vistasTxtAsignatura.clear();
 }
 
 void VistaTxtAsignaturas::refrescar()
 {
+    // Se limpian las vistas anteriores.
+    for (std::list<VistaTxtAsignatura*>::iterator i = vistasTxtAsignatura.begin(); i != vistasTxtAsignatura.end(); i++)
+        delete *i;
+    vistasTxtAsignatura.clear();
 
     std::cout << "\e[45m\e[1m\e[37m" << rellenar("ASIGNATURAS", " ", columnas, true, true) << "\e[0m" << std::endl;
     std::cout << "\e[45m\e[1m\e[37m" << rellenar("", "-", columnas, true, false) << "\e[0m" << std::endl;
@@ -20,6 +40,8 @@ void VistaTxtAsignaturas::refrescar()
         std::list<Asignatura*>::const_iterator i = asignaturas->obtenerAsignaturas().begin();
         for (; i != asignaturas->obtenerAsignaturas().end(); i++)
         {
+            VistaTxtAsignatura *vistaTxtAsignatura = new VistaTxtAsignatura(*i);
+            vistasTxtAsignatura.push_back(vistaTxtAsignatura);
             std::cout << "\e[45m\e[1m\e[37m" << rellenar(" " + (*i)->getNombre(), " ", columnas, false, true) << "\e[0m" << std::endl;
         }
     }
--- trunk/Practica-pei/Codigo/Vista-txt/VistaTxtAsignaturas.h
+++ trunk/Practica-pei/Codigo/Vista-txt/VistaTxtAsignaturas.h
@@ -3,14 +3,18 @@
 
 #include "Vista.h"
 #include "Asignaturas.h"
+#include <list>
+#include "VistaTxtAsignatura.h"
 
 class VistaTxtAsignaturas : public Vista
 {
 public:
-    VistaTxtAsignaturas(Asignaturas *asignaturas);
+    VistaTxtAsignaturas(Asignaturas *asignaturas = NULL);
+    ~VistaTxtAsignaturas();
     void refrescar();
     
 private:
+    std::list<VistaTxtAsignatura*> vistasTxtAsignatura;
     const static int columnas = 80;
 };
 
