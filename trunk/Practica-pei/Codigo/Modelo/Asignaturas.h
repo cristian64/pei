f08a1b1a136d4238255fd8a3bd6949a7e65d38b1
cristian64@gmail.com <cristian64@gmail.com@928e6174-d238-cfd1-98c4-7db66ba74160>
2011-04-17 14:53:23 +0000

    Añadida la vista de asignaturas en Qt. Se pueden crear, guardar y abrir paquetes de asignaturas en la vista de Qt.

--- trunk/Practica-pei/Codigo/Modelo/Asignaturas.h
+++ trunk/Practica-pei/Codigo/Modelo/Asignaturas.h
@@ -8,21 +8,27 @@
 class Asignaturas : public Modelo
 {
 public:
-    Asignaturas();
-    ~Asignaturas();
+	Asignaturas();
+	~Asignaturas();
 
-    void anadirAsignatura(Asignatura *asignatura);
-    void quitarAsignatura(Asignatura *asignatura);
-    const std::list<Asignatura*>& obtenerAsignaturas() const;
+	void anadirAsignatura(Asignatura *asignatura);
+	void quitarAsignatura(Asignatura *asignatura);
+	const std::list<Asignatura*>& obtenerAsignaturas() const;
 
-    std::string toXml() const;
-    void fromXml(const std::string &xml);
+	void limpiar();
 
-    void guardar(const std::string &rutaFichero) const;
-    void cargar(const std::string &rutaFichero);
-    
+	std::string toXml() const;
+	void fromXml(const std::string &xml);
+
+	void guardar(const std::string &rutaFichero) const;
+	void cargar(const std::string &rutaFichero);
+
+	std::string getRutaFichero() const;
+	void setRutaFichero(const std::string &rutaFichero);
+
 private:
-    std::list<Asignatura*> asignaturas;
+	mutable std::string rutaFichero;
+	std::list<Asignatura*> asignaturas;
 };
 
 #endif	/* ASIGNATURAS_H */
