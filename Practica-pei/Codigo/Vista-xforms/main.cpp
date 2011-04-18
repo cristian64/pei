6807ba938f6c8d5e16c420d0ff3e9eaa1bf3350e
cristian64@gmail.com <cristian64@gmail.com@928e6174-d238-cfd1-98c4-7db66ba74160>
2011-04-18 19:33:49 +0000

    Eliminada la Calculadora en Xforms y añadido un nuevo formulario lo más parecido posible a la vista de Qt. Falta que haga cosas.

--- Practica-pei/Codigo/Vista-xforms/main.cpp
+++ Practica-pei/Codigo/Vista-xforms/main.cpp
@@ -1,43 +1,26 @@
 #include "Formulario.h"
 
-int main(int argc, char **argv)
-{    
-    FD_Calculadora *fd_Calculadora;
+int
+main( int    argc,
+      char * argv[ ] )
+{
+    FD_Formulario *fd_Formulario;
 
-    fl_initialize(&argc, argv, 0, 0, 0 );
-    fd_Calculadora = create_form_Calculadora( );
+    fl_initialize( &argc, argv, 0, 0, 0 );
+    fd_Formulario = create_form_Formulario( );
 
     /* Fill-in form initialization code */
 
     /* Show the first form */
 
-    fl_show_form( fd_Calculadora->Calculadora, FL_PLACE_CENTERFREE, FL_FULLBORDER, "Calculadora" );
-    fd_Calculadora->button1->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button2->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button3->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button4->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button5->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button6->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button7->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button8->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button9->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button0->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button_plus->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button_minus->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button_div->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button_mult->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button_equal->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button_c->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button_clr->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button_min->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button_mr->u_vdata = fd_Calculadora->lcd;
-    fd_Calculadora->button_sign->u_vdata = fd_Calculadora->lcd;
+    fl_show_form( fd_Formulario->Formulario, FL_PLACE_CENTERFREE, FL_FULLBORDER, "Vista Xforms" );
 
     fl_do_forms( );
 
-    if ( fl_form_is_visible( fd_Calculadora->Calculadora ) )
-        fl_hide_form( fd_Calculadora->Calculadora );
-    fl_free( fd_Calculadora );
+    if ( fl_form_is_visible( fd_Formulario->Formulario ) )
+        fl_hide_form( fd_Formulario->Formulario );
+    fl_free( fd_Formulario );
     fl_finish( );
-}
 
+    return 0;
+}
