74d148105ec45dfc142700576a75e690d6df7981
cristian64@gmail.com <cristian64@gmail.com@928e6174-d238-cfd1-98c4-7db66ba74160>
2011-04-20 15:31:25 +0000

    Ya se desactiva el formulario de Xforms cuando no hay ningnua asignatura seleccionada.

--- trunk/Practica-pei/Codigo/Vista-xforms/VistaXformsAsignatura.cpp
+++ trunk/Practica-pei/Codigo/Vista-xforms/VistaXformsAsignatura.cpp
@@ -61,5 +61,45 @@ void VistaXformsAsignatura::refrescar()
                 fl_add_browser_line(formulario->browser_asignatura, (*i)->getDescripcion().c_str());
             }
         }
+
+        fl_activate_object(formulario->browser_asignatura);
+        fl_activate_object(formulario->button_plus);
+        fl_activate_object(formulario->button_minus);
+        fl_activate_object(formulario->button_profesores);
+        fl_activate_object(formulario->button_companeros);
+        fl_activate_object(formulario->button_sesiones);
+        fl_activate_object(formulario->button_citas);
+        fl_activate_object(formulario->button_notas);
+
+        if (fl_get_button(formulario->button_profesores) == 0)
+            seleccionar_pestana(formulario->button_profesores);
+        else if (fl_get_button(formulario->button_companeros) == 0)
+            seleccionar_pestana(formulario->button_companeros);
+        else if (fl_get_button(formulario->button_sesiones) == 0)
+            seleccionar_pestana(formulario->button_sesiones);
+        else if (fl_get_button(formulario->button_citas) == 0)
+            seleccionar_pestana(formulario->button_citas);
+        else if (fl_get_button(formulario->button_notas) == 0)
+            seleccionar_pestana(formulario->button_notas);
+    }
+    else
+    {
+        fl_deactivate_object(formulario->browser_asignatura);
+        fl_deactivate_object(formulario->button_plus);
+        fl_deactivate_object(formulario->button_minus);
+        fl_deactivate_object(formulario->button_profesores);
+        fl_deactivate_object(formulario->button_companeros);
+        fl_deactivate_object(formulario->button_sesiones);
+        fl_deactivate_object(formulario->button_citas);
+        fl_deactivate_object(formulario->button_notas);
+
+        fl_set_object_lcolor(formulario->browser_asignatura, 31);
+        fl_set_object_lcolor(formulario->button_plus, 31);
+        fl_set_object_lcolor(formulario->button_minus, 31);
+        fl_set_object_lcolor(formulario->button_profesores, 31);
+        fl_set_object_lcolor(formulario->button_companeros, 31);
+        fl_set_object_lcolor(formulario->button_sesiones, 31);
+        fl_set_object_lcolor(formulario->button_citas, 31);
+        fl_set_object_lcolor(formulario->button_notas, 31);
     }
 }
