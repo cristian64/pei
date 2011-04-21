cbaac4112f01b3f0f6590d759619e6adc9c13243
cristian64@gmail.com <cristian64@gmail.com@928e6174-d238-cfd1-98c4-7db66ba74160>
2011-04-21 02:26:47 +0000

    Reemplazado pthread por un QTimer de Qt. En cada intervalo se realiza un fl_check_forms para repitar/evaluar eventos de Xforms.

--- trunk/Practica-pei/Codigo/Vista-multiple/main.cpp
+++ trunk/Practica-pei/Codigo/Vista-multiple/main.cpp
@@ -1,75 +1,27 @@
 #include <QtGui/QApplication>
 #include "Asignaturas.h"
 #include "MainWindow.h"
-#include "Formulario.h"
-#include "VistaXformsAsignaturas.h"
-#include "VistaXformsAsignatura.h"
-#include "../Vista-xforms/VistaXformsAsignaturas.h"
-#include <pthread.h>
-#include <iostream>
-using namespace std;
-
-typedef struct
-{
-    int argc;
-    char **argv;
-    Asignaturas *asignaturas;
-} Parameters;
-
-void* vistaXforms(void *parameters)
-{
-    FD_Formulario *fd_Formulario;
-
-    fl_initialize(&(((Parameters*) parameters)->argc), ((Parameters*) parameters)->argv, 0, 0, 0 );
-    fd_Formulario = create_form_Formulario( );
-
-    // Se crea el modelo y la vista.
-    VistaXformsAsignatura vistaXformsAsignatura;
-    vistaXformsAsignatura.formulario = fd_Formulario;
-    VistaXformsAsignaturas vistaXformsAsignaturas;
-    vistaXformsAsignaturas.formulario = fd_Formulario;
-    vistaXformsAsignaturas.vistaXformsAsignatura = &vistaXformsAsignatura;
-    vistaXformsAsignaturas.ponerModelo(((Parameters*) parameters)->asignaturas);
-    vistaXformsAsignaturas.refrescar();
-
-    // Se asocia a cada objeto del formulario las vistas para que puedan manipular el modelo de cada vista.
-    fd_Formulario->vdata = &vistaXformsAsignaturas;
-    fd_Formulario->cdata = (char*) (&vistaXformsAsignatura);
-
-    /* Show the first form */
-
-    fl_show_form( fd_Formulario->Formulario, FL_PLACE_CENTERFREE, FL_FULLBORDER, "Vista Xforms" );
-
-    fl_do_forms( );
-
-    if ( fl_form_is_visible( fd_Formulario->Formulario ) )
-        fl_hide_form( fd_Formulario->Formulario );
-    fl_free( fd_Formulario );
-    fl_finish( );
-    
-    pthread_exit(0);
-}
+#include "XformsTimer.h"
 
 int main(int argc, char **argv)
 {
     Asignaturas asignaturas;
     asignaturas.cargar("ejemplo1.xml");
-    asignaturas.refrescarVistas();
+
     QApplication a(argc, argv);
+
+    // Se crean dos ventanas de la Vista Qt.
     MainWindow w(&asignaturas);
     w.show();
     MainWindow w2(&asignaturas);
     w2.show();
-    
-    Parameters parameters;
-    parameters.argc = argc;
-    parameters.argv = argv;
-    parameters.asignaturas = &asignaturas;
-    
-    pthread_t vistaQt_thread;
-    pthread_create(&vistaQt_thread, NULL, vistaXforms, &parameters);
+
+    // Se crea un timer, cuyo constructor crea una Vista Xforms. En cada intervalo, se realiza un fl_check_forms.
+    // Se consigue así que no haya corrupción de memoria (antes usaba pthread y a veces daba un cascazo increíble).
+    XformsTimer timer(argc, argv, &asignaturas);
+    timer.setInterval(100);
+    timer.start();
     
     return a.exec();
-    pthread_join(vistaQt_thread, NULL);
 }
 
