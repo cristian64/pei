43c1a36f8af8e522a98faa36d4eb5376e592c603
cristian64@gmail.com <cristian64@gmail.com@928e6174-d238-cfd1-98c4-7db66ba74160>
2011-04-22 12:22:40 +0000

    Ya se pueden ver, añadir, editar y borrar sesiones y compañeros.

--- trunk/Practica-pei/Codigo/Vista-gtkmm/VistaGtkmmAsignatura.h
+++ trunk/Practica-pei/Codigo/Vista-gtkmm/VistaGtkmmAsignatura.h
@@ -16,6 +16,11 @@ private:
     Gtk::TreeModel::ColumnRecord columnas;
     Gtk::TreeModelColumn<Glib::ustring> columnaNombre;
     Gtk::TreeModelColumn<Glib::ustring> columnaEmail;
+    Gtk::TreeModelColumn<Glib::ustring> columnaTipo;
+    Gtk::TreeModelColumn<Glib::ustring> columnaLugar;
+    Gtk::TreeModelColumn<Glib::ustring> columnaDia;
+    Gtk::TreeModelColumn<Glib::ustring> columnaHoraInicio;
+    Gtk::TreeModelColumn<Glib::ustring> columnaHoraFin;
 
     Gtk::Button *buttonAnadirProfesor;
     Gtk::Button *buttonQuitarProfesor;
@@ -25,13 +30,47 @@ private:
     Gtk::Dialog *dialogProfesor;
     Gtk::Entry *entryNombreProfesor;
     Gtk::Entry *entryEmailProfesor;
-
     void anadirProfesor();
     void quitarProfesor();
     void seleccionarProfesor();
     void editarProfesor(const Gtk::TreeModel::Path &path, Gtk::TreeViewColumn *col);
-
     std::map<Profesor*, Gtk::TreeModel::Row> vinculosProfesores;
+
+    Gtk::Button *buttonAnadirCompanero;
+    Gtk::Button *buttonQuitarCompanero;
+    Gtk::TreeView *treeviewCompaneros;
+    Glib::RefPtr<Gtk::ListStore> treemodelCompaneros;
+    Glib::RefPtr<Gtk::TreeSelection> treeselectionCompaneros;
+    Gtk::Dialog *dialogCompanero;
+    Gtk::Entry *entryNombreCompanero;
+    Gtk::Entry *entryEmailCompanero;
+    void anadirCompanero();
+    void quitarCompanero();
+    void seleccionarCompanero();
+    void editarCompanero(const Gtk::TreeModel::Path &path, Gtk::TreeViewColumn *col);
+    std::map<Companero*, Gtk::TreeModel::Row> vinculosCompaneros;
+
+    Gtk::Button *buttonAnadirSesion;
+    Gtk::Button *buttonQuitarSesion;
+    Gtk::TreeView *treeviewSesiones;
+    Glib::RefPtr<Gtk::ListStore> treemodelSesiones;
+    Glib::RefPtr<Gtk::TreeSelection> treeselectionSesiones;
+    Gtk::Dialog *dialogSesion;
+    Gtk::RadioButton *radiobuttonTeoria;
+    Gtk::RadioButton *radiobuttonPracticas;
+    Gtk::Entry *entryLugar;
+    Gtk::ComboBox *comboboxDia;
+    Gtk::SpinButton *spinbuttonHora;
+    Gtk::SpinButton *spinbuttonMinuto;
+    Gtk::SpinButton *spinbuttonSegundo;
+    Gtk::SpinButton *spinbuttonHora2;
+    Gtk::SpinButton *spinbuttonMinuto2;
+    Gtk::SpinButton *spinbuttonSegundo2;
+    void anadirSesion();
+    void quitarSesion();
+    void seleccionarSesion();
+    void editarSesion(const Gtk::TreeModel::Path &path, Gtk::TreeViewColumn *col);
+    std::map<Sesion*, Gtk::TreeModel::Row> vinculosSesiones;
 };
 
 #endif	/* VISTAGTKMMASIGNATURA_H */
