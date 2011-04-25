0f1961c6f6358cd26311137cf27034db3ca847f4
cristian64@gmail.com <cristian64@gmail.com@928e6174-d238-cfd1-98c4-7db66ba74160>
2011-04-25 12:42:19 +0000

    Ahora la vista múltiple refresca Xforms un poquito más rápido y sólo abre una vista de Qt.

--- Practica-pei/Codigo/Vista-multiple/main.cpp
+++ Practica-pei/Codigo/Vista-multiple/main.cpp
@@ -13,13 +13,11 @@ int main(int argc, char **argv)
     // Se crean dos ventanas de la Vista Qt.
     MainWindow w(&asignaturas);
     w.show();
-    MainWindow w2(&asignaturas);
-    w2.show();
 
     // Se crea un timer, cuyo constructor crea una Vista Xforms. En cada intervalo, se realiza un fl_check_forms.
     // Se consigue así que no haya corrupción de memoria (antes usaba pthread y a veces daba un cascazo increíble).
     XformsTimer timer(argc, argv, &asignaturas);
-    timer.setInterval(100);
+    timer.setInterval(10);
     timer.start();
     
     return a.exec();
